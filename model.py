5aee8b231c9ef584e9c70956d7bb3efd91525c6e
Brayan7546 <99565885+Brayan7546@users.noreply.github.com>
2022-02-17 17:25:04 -0500

    Update model.py

--- model.py
+++ model.py
@@ -23,3 +23,26 @@ class Person(object):
                 result.append(person)
                 line_count += 1
         return result
+
+    class Document(object):
+
+    def __init__(self, title=None, authors=None, pub_udate=None, id_book=None, edition=None, no_pag=None):
+        self.__title = title
+        self.__id_book = id_book
+        self.__authors = authors
+        self.__pub_udate = pub_udate
+        self.__edition= edition
+        self.__no_pag= no_pag
+        
+
+    @classmethod
+    def get_all(self):
+        result = list()
+        with open('db.txt', mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            line_count = 0
+            for row in csv_reader:
+                document = Document(row['title'], row['id_book'], row['authors'], row['pub_udate'], row['edition'],row['no_pag'])
+                result.append(person)
+                line_count += 1
+        return result
