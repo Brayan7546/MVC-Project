b0e010d76ae4625290fd290bddb24064fd0de7d1
Brayan7546 <99565885+Brayan7546@users.noreply.github.com>
2022-02-17 17:48:52 -0500

    Update view.py

--- view.py
+++ view.py
@@ -1,15 +1,15 @@
-from model import Person
+from model import Author
+from model import Document
 
-
-def show_all_view(list):
+def show_authors_view(list):
     print('In our db we have %i users. Here they are:' % len(list))
     for item in list:
         print(item.name())
 
 
-def start_view():
-    print('MVC - the simplest example')
-    print('Do you want to see everyone in my db?[y/n]: ')
+def start_authors_view():
+    #print('MVC - the simplest example')
+    print('Do you want to see all the authors in my db?[y/n]: ')
 
 
 def end_view():
