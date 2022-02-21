b74aa3a78d92f41b9a1dc0d78ffa95344c17a64f
Brayan7546 <99565885+Brayan7546@users.noreply.github.com>
2022-02-21 18:29:07 -0500

    Update view.py

--- view.py
+++ view.py
@@ -1,11 +1,22 @@
 from model import Author
 from model import Document
 
+def menu_view():
+    print('1. Agregar un Documento')
+    print('2. Mostrar documentos')
+    print('3. Agregar autores')
+    print('4. Mostrar Autores')
+    
+
 def show_authors_view(list):
     print('In our db we have %i users. Here they are:' % len(list))
     for item in list:
         print(item.name())
 
+def show_documents_view(list):
+    print('In our db we have %i documents. Here they are:' % len(list))
+    for item in list:
+        print(item.title())
 
 def start_authors_view():
     #print('MVC - the simplest example')
