6ae0d269e01cf20f6f356ff6b8ff414ac510c146
Brayan7546 <99565885+Brayan7546@users.noreply.github.com>
2022-02-21 18:25:12 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -4,6 +4,18 @@ from model import Author
 from model import Document
 import view
 
+def start():
+    view.menu_view()
+    _input = input()
+    #if _input == '1':
+    
+    if _input == '2':
+        return show_documents()
+    if _input == '4':
+        return show_authors()
+    else:
+        print('opcion invalida')
+
 def show_authors():
     #gets list of all authors objects
     authors_in_db = Author.get_all()
@@ -12,7 +24,7 @@ def show_authors():
 
 
 def start_authors():
-    view.start_view()
+    view.start_authors_view()
     _input = input()
     if _input == 'y':
         return show_authors()
@@ -21,15 +33,16 @@ def start_authors():
 
 
 if __name__ == "__main__":
-    start()
+    view.menu_view()
+    
       
     
       
 def show_documents():
     #gets list of all Documents objects
-    document_in_db = Document.get_all()
+    documents_in_db = Document.get_all()
     #calls view
-    return view.show_document_view(document_in_db)
+    return view.show_documents_view(documents_in_db)
 
 def start_documents():
     view.start_documents_view()
